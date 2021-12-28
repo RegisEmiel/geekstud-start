2fe62980a0e124ce45362da651bbf5677c57d5e7
RegisEmiel <regisemiel@yandex.ru>
2021-12-29 00:06:02 +0300

    Entities added commit

---
+++ src/main/java/com/geekbrains/geekstudents/entities/Category.java
@@ -0,0 +1,38 @@
+package com.geekbrains.geekstudents.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    String name;
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
